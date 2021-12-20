9dc09a613f196cd5c419b44bd4179e1ab45df5a7
Trifonov, Vladimir [JANUS] <VTrifono@its.jnj.com>
2021-12-19 22:11:41 -0800

    work

--- work/playground1.py
+++ work/playground1.py
@@ -4,12 +4,16 @@ import numpy as np
 import itertools as it
 
 # %%
-x = np.array(['abc', '', 'def'], dtype=object)
-x = sparse.COO.from_numpy(x, fill_value='')
-sparse.save_npz('xxx.npz', x)
-x = sparse.load_npz('xxx.npz')
+x = sparse.random(
+    (10, 10, 10), 
+    nnz=int(500), 
+    data_rvs=lambda nnz: np.random.random(nnz)
+)
 
 # %%
-np.broadcast_to(x, (100,1))
+c1 = np.ravel_multi_index(x.coords, x.shape)
+c2 = np.unravel_index(c1, shape=(1000,))
+x1 = x.reshape((1000,))
+c3 = np.ravel_multi_index(x1.coords, x1.shape)
 
-x = np.where([False])[0]
+np.all(c1==c3)
