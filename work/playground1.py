6042698e3d99537d0453eac46cf76732709b1f62
Vladimir Trifonov <v_trifonov@yahoo.com>
2021-12-29 14:48:26 -0800

    work

--- work/playground1.py
+++ work/playground1.py
@@ -2,37 +2,42 @@
 import sparse
 import numpy as np
 import itertools as it
-from zarr.sparse.sparse1 import Sparse
-from zarr.sparse.sparse1 import _conv_list, _conv_single, _conv_slice
+from zarr.sparse.sparse1 import array
+import pickle
+from pathlib import Path
 
 #%%
 
-np.broadcast_to(
-    np.array([]).reshape((2,0)),
-    (2,1)
-)
+x3 = Path('xxx.pickle')
+if not x3.exists():
+    x1 = np.random.random(int(10e6))
+    x2 = np.random.randint(int(1e9), size=len(x1))
+    x2 = np.sort(x2)
+    x2 = np.unravel_index(x2, (1000,)*3)
+    with x3.open('wb') as file:
+        pickle.dump(
+            array(x1, x2, shape=(1000,)*3),
+            file
+        )
+with x3.open('rb') as file:
+    x3 = pickle.load(file)
+
 
 #%%
+%timeit x4 = x3[(slice(100,200),)*3]
 
-x = Sparse(shape=(3,3), dtype=np.float64)
-x[0,0] = Sparse(data=[1], coords=[[0],[0]])
-x[0,1:] = Sparse(data=[1,2], coords=[[0, 0],[0,1]])
-
-np.broadcast_shapes((2,), (2,1))
+#%%
+%timeit x4 = x3[(list(range(100, 200)),)*3]
 
 
-# %%
+#%%
+r = [[1], [2,3], [4], [5,6,7], [10,11]]
 
-c = _conv_list(np.array([0, 1, 3]))
-c.fwd(np.array([1, 1, 0, 3, 5, -2]))
-c.rev(np.array([0, 2, 1, 4, -1, 5]))
-
-c = _conv_slice(slice(10, 20, 2))
-c.fwd(np.array([10, 12, 13, 14, 22, 8]))
-c.rev(np.array([0, 1, 2, 5, 4, -2, 6]))
-
-c = _conv_single(2)
-c.fwd(np.array([2, 3, 2, 4, 2]))
-c.rev(np.array([0, 1, -1, 2, 0, 2]))
+%timeit np.array(list(it.product(*r))).T
 
 # %%
+
+x = np.empty((2,2), dtype=object)
+x[0,:] = [[1,2], [1]]
+x[1,:] = [[], [1,2,3]]
+np.vectorize(len)(x).prod(axis=0).sum()
